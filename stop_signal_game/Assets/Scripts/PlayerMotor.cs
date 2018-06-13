10ea1f7fe399e34d3f58ef2f7afd9a7dc59d58d4
Sarah Vedress <>
2018-06-13 11:10:07 -0600

    added a player that can run foreward, and a plane to run on

---
+++ stop_signal_game/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+
+	private CharacterController controller;
+
+	[Range(1.0f, 10.0f)] 
+	public float speed = 5.0f; 
+
+	void Start ()
+	{
+		controller = GetComponent<CharacterController>(); 
+	}
+	
+	void Update ()
+	{
+		
+		controller.Move((Vector3.forward * speed) * Time.deltaTime); 
+	}
+}
